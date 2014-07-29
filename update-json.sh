ae53b160052ea6b5f3d542d0a7956bb18ead6b50
ruben <smartgit@r.z11.de>
2014-07-29 15:48:04 +0000

    Added logging function to update-json.sh

--- update-json.sh
+++ update-json.sh
@@ -3,3 +3,9 @@
 # call this script regularly as a cron job to get the latest alfred.json file
 
 wget http://freifunk.in-kiel.de/alfred.json -O alfred.json
+mkdir -p logs
+cp alfred.json logs/alfred_`date "+%y%m%d-%H%M%S"`.json
+
+# to download the alfred json file every 10 minutes add this to your crontab:
+#*/10 * * * * some_user cd /path/to/gluon-alfred-vis; bash update-json.sh > /dev/null
+
